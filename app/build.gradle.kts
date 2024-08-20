37fb31e70b68746b24a1d54cb5b4daaf29d5ea4f
Batool Personal <batoolhammouda45@gmail.com>
2024-08-20 12:20:39 +0300

    Splash Screen impl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,14 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
 }
