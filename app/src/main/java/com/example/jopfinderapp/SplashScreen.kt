aa77769257033d49b8020653706baa538d7f85ce
Batool Personal <batoolhammouda45@gmail.com>
2024-08-21 12:16:21 +0300

    CR changes 2

--- app/src/main/java/com/example/jopfinderapp/SplashScreen.kt
+++ app/src/main/java/com/example/jopfinderapp/SplashScreen.kt
@@ -1,18 +1,17 @@
 package com.example.jopfinderapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +30,12 @@ class SplashScreen : AppCompatActivity() {
             )
         }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent= Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        },3000
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000
 
         )
 
