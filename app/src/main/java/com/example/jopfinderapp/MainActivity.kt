237ae984555958b2763d0f5376200622cc2d1b05
Batool Personal <batoolhammouda45@gmail.com>
2024-08-23 14:15:47 +0300

    CR2 changes
    Changing the email validation method to be private.

--- app/src/main/java/com/example/jopfinderapp/MainActivity.kt
+++ app/src/main/java/com/example/jopfinderapp/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun isValidEmail(email: String): Boolean {
+   private fun isValidEmail(email: String): Boolean {
         val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
         return email.matches(emailRegex.toRegex())
     }
